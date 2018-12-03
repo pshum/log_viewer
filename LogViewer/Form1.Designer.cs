da31a8bf33d4488665b835f786e313cde8ed711c
Pong Shum <45453731+pshum@users.noreply.github.com>
2018-12-03 11:05:29 +0800

    First Log

---
+++ LogViewer/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace LogViewer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFolderToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fbDia = new System.Windows.Forms.FolderBrowserDialog();
+            this.lstFile = new System.Windows.Forms.ListBox();
+            this.lstSec = new System.Windows.Forms.ListBox();
+            this.lstWarn = new System.Windows.Forms.ListBox();
+            this.grpBox = new System.Windows.Forms.GroupBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1021, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openFolderToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // openFolderToolStripMenuItem
+            // 
+            this.openFolderToolStripMenuItem.Name = "openFolderToolStripMenuItem";
+            this.openFolderToolStripMenuItem.Size = new System.Drawing.Size(139, 22);
+            this.openFolderToolStripMenuItem.Text = "&Open Folder";
+            this.openFolderToolStripMenuItem.Click += new System.EventHandler(this.openFolderToolStripMenuItem_Click);
+            // 
+            // lstFile
+            // 
+            this.lstFile.FormattingEnabled = true;
+            this.lstFile.Location = new System.Drawing.Point(12, 27);
+            this.lstFile.Name = "lstFile";
+            this.lstFile.Size = new System.Drawing.Size(230, 251);
+            this.lstFile.TabIndex = 1;
+            this.lstFile.SelectedIndexChanged += new System.EventHandler(this.lstFile_SelectedIndexChanged);
+            // 
+            // lstSec
+            // 
+            this.lstSec.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.lstSec.FormattingEnabled = true;
+            this.lstSec.Location = new System.Drawing.Point(12, 287);
+            this.lstSec.Name = "lstSec";
+            this.lstSec.Size = new System.Drawing.Size(229, 316);
+            this.lstSec.TabIndex = 2;
+            this.lstSec.SelectedIndexChanged += new System.EventHandler(this.lstSec_SelectedIndexChanged);
+            // 
+            // lstWarn
+            // 
+            this.lstWarn.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lstWarn.FormattingEnabled = true;
+            this.lstWarn.HorizontalScrollbar = true;
+            this.lstWarn.Location = new System.Drawing.Point(249, 79);
+            this.lstWarn.Name = "lstWarn";
+            this.lstWarn.Size = new System.Drawing.Size(760, 524);
+            this.lstWarn.TabIndex = 3;
+            // 
+            // grpBox
+            // 
+            this.grpBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.grpBox.Location = new System.Drawing.Point(249, 27);
+            this.grpBox.Name = "grpBox";
+            this.grpBox.Size = new System.Drawing.Size(760, 43);
+            this.grpBox.TabIndex = 4;
+            this.grpBox.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1021, 608);
+            this.Controls.Add(this.grpBox);
+            this.Controls.Add(this.lstWarn);
+            this.Controls.Add(this.lstSec);
+            this.Controls.Add(this.lstFile);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openFolderToolStripMenuItem;
+        private System.Windows.Forms.FolderBrowserDialog fbDia;
+        private System.Windows.Forms.ListBox lstFile;
+        private System.Windows.Forms.ListBox lstSec;
+        private System.Windows.Forms.ListBox lstWarn;
+        private System.Windows.Forms.GroupBox grpBox;
+    }
+}
+
---
+++ LogViewer/Form1.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO;
+using System.Collections;
+
+namespace LogViewer
+{
+    public partial class Form1 : Form
+    {
+        private string[] _fileCheck = { "vrmonitor.txt", "vrmonitor.previous.txt", "vrcompositor.txt",
+            "vrcompositor.previous.txt", "vrclient_vrcompositor.txt", "vrclient_vrmonitor.txt",
+            "vrserver.txt", "vrserver.previous.txt",
+            "vrclient_CurseofDavyJones.txt", "vrclient_DeadwoodMansion.txt"};
+        private string _selectedPath = "";
+        private ArrayList _fileCache = new ArrayList();
+        private ArrayList _libLog = new ArrayList();        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openFolderToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DialogResult res =  fbDia.ShowDialog();
+            if(res == DialogResult.OK)
+            {
+                resetList();
+                
+                //string _fold = fbDia.SelectedPath;
+                _selectedPath = fbDia.SelectedPath;
+
+                this.Text = "Working Folder : " +_selectedPath;
+
+                for (int vi  = 0; vi < _fileCheck.Length; ++vi)
+                {
+                    string _file = _fileCheck[vi];
+                    string _chk = "OK";
+                    if (!File.Exists(_selectedPath + "\\" + _file))
+                    {
+                        _chk = "Failed";
+                    }
+                    lstFile.Items.Add(_file + " : " + _chk);
+                }
+                Console.WriteLine(fbDia.SelectedPath);
+            }
+        }
+
+        private void lstFile_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (lstFile.SelectedIndex == -1) return;
+
+            string _filePath = _selectedPath + "\\" + _fileCheck[lstFile.SelectedIndex];
+            
+            if(File.Exists(_filePath))
+            {
+                string line;
+                lstSec.Items.Clear();
+                lstWarn.Items.Clear();
+                grpBox.Controls.Clear();
+
+                _fileCache.Clear();
+                StreamReader _sr = new StreamReader(_filePath);
+                while((line = _sr.ReadLine()) != null){
+                    if (readSecSt(line))
+                    {
+                        if(_fileCache.Count == 0) {
+                            _fileCache.Add(new ArrayList());
+                            string[] lineStr = line.Split('-');
+                            lstSec.Items.Add("Section " + _fileCache.Count + " (" + lineStr[0] + ")");
+                        }
+                        else
+                        {
+                            ArrayList _secList = (ArrayList)_fileCache[(_fileCache.Count - 1)];
+                            if(_secList.Count > 0) {
+                                _fileCache.Add(new ArrayList());
+                                string[] lineStr = line.Split('-');
+                                lstSec.Items.Add("Section " + _fileCache.Count + " ("+ lineStr[0] +")");
+                            }
+                            
+                        }                       
+                    }
+                    else
+                    {
+                        if (_fileCache.Count > 0)
+                        {
+                            ArrayList _secList = (ArrayList)_fileCache[(_fileCache.Count - 1)];
+                            _secList.Add(catLogString(line));
+                        }
+                    }
+                }
+            }            
+        }
+
+        private bool readSecSt(string lineStr)
+        {
+            if (lineStr.Contains("==========="))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private void lstSec_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (lstSec.SelectedIndex == -1) return;
+
+            lstWarn.Items.Clear();
+            _libLog.Clear();
+
+            ArrayList _secContent = (ArrayList)_fileCache[lstSec.SelectedIndex];
+
+            foreach( string _line in _secContent)
+            {
+                lstWarn.Items.Add(_line);
+                string tag = checkHashTab(_line);
+                if(tag != "")
+                {
+                    int _in = findLibIndex(tag);
+                    KeyValuePair<string, ArrayList> _lstLog;
+                    if(_in >= 0)
+                    {
+                        _lstLog = (KeyValuePair<string, ArrayList>)_libLog[_in];
+                    }
+                    else
+                    {
+                        _lstLog = new KeyValuePair<string, ArrayList>(tag, new ArrayList());
+                        _libLog.Add(_lstLog);
+                    }
+                    _lstLog.Value.Add(_line);
+                }
+            }
+
+            showButtonByTag();
+            //lstWarn.Items.AddRange(_secContent.ToArray());
+
+            
+        }
+
+        private int findLibIndex(string tag)
+        {
+            for(int vi = 0; vi < _libLog.Count; ++vi)
+            {
+                KeyValuePair<string, ArrayList> _lib = (KeyValuePair<string, ArrayList>)_libLog[vi];
+                if (_lib.Key.Equals(tag))
+                {
+                    return vi;
+                }
+            }
+            return -1;
+        }
+
+        private void showButtonByTag()
+        {
+            Point pt = new Point(6, 14);           
+            grpBox.Controls.Clear();
+
+            foreach(KeyValuePair<string, ArrayList> _libKey in _libLog)
+            {
+                Button btn = new Button();
+                btn.Text = _libKey.Key;
+                btn.Left = pt.X + grpBox.Controls.Count * btn.Size.Width ;
+                btn.Top = pt.Y;
+                btn.Tag = grpBox.Controls.Count;
+                btn.Click += Btn_Click;
+                grpBox.Controls.Add(btn);              
+            }
+
+            //for All Button
+            Button btnAll = new Button();
+            btnAll.Text = "All";
+            btnAll.Left = pt.X + grpBox.Controls.Count * btnAll.Size.Width;
+            btnAll.Top = pt.Y;
+            btnAll.Tag = grpBox.Controls.Count;
+            btnAll.Click += BtnAll_Click; ;
+            grpBox.Controls.Add(btnAll);
+        }
+
+        private void BtnAll_Click(object sender, EventArgs e)
+        {
+            lstWarn.Items.Clear();
+
+            ArrayList _lst = (ArrayList)_fileCache[lstSec.SelectedIndex];
+            lstWarn.Items.AddRange(_lst.ToArray());
+        }
+
+        private void Btn_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            lstWarn.Items.Clear();
+
+            KeyValuePair<string, ArrayList> _lib = (KeyValuePair<string, ArrayList>)_libLog[(int)btn.Tag];
+            lstWarn.Items.AddRange(_lib.Value.ToArray());
+        }
+
+        private string checkHashTab(string line)
+        {
+            string res = "";
+            if(line.Length > 0 && line[0].Equals('['))
+            {
+                int _end = line.IndexOf(']')-1;
+                res = line.Substring(1, _end);
+            }
+            return res;
+        }
+        
+        private string catLogString(string logLine)
+        {
+            string res = "";
+            string[] lineStr = logLine.Split('-');
+            for (int vi = 1; vi < lineStr.Length; ++vi)
+            {
+                res += lineStr[vi];
+            }
+
+            res = res.TrimStart(' ');
+            return res;
+        }
+
+        private void resetList()
+        {
+            lstSec.Items.Clear();
+            lstFile.Items.Clear();
+            lstWarn.Items.Clear();
+            _fileCache.Clear();
+            _libLog.Clear();
+        }
+    }
+    
+}
